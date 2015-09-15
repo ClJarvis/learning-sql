da68d9c9c58ea7c6a44b82a1e126d458575355ce
Chris Jarvis <clj@ollure.com>
2015-09-14 20:49:47 -0500

    seven passing

---
+++ 7.sql
@@ -0,0 +1,6 @@
+SELECT I.InvoiceId, I.InvoiceDate, I.CustomerId,  E.FirstName, E.LastName
+FROM Invoice  I
+JOIN Customer C
+ON C.CustomerId  == I.CustomerId
+JOIN Employee E ON
+E.EmployeeId == C.SupportRepId
