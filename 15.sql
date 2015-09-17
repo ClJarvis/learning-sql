2686e68e08ae917c7fc76754663fa385af07e879
Chris Jarvis <clj@ollure.com>
2015-09-16 23:50:11 -0500

    15 queries asked

---
+++ 15.sql
@@ -0,0 +1,7 @@
+
+SELECT SUM(PlaylistTrack.TrackId) FROM Playlist, PlaylistTrack; --277202106 ?
+
+/* SELECT  P.Name, COUNT(PlaylistTrack.TrackId)
+FROM Playlist P, PlaylistTrack
+GROUP BY TrackId */
+
