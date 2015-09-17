aec1747ed081b7fcb4c17843417de77fd59ed308
Chris Jarvis <clj@ollure.com>
2015-09-17 10:26:08 -0500

    working on 15

--- 15.sql
+++ 15.sql
@@ -1,6 +1,11 @@
 
 SELECT SUM(PlaylistTrack.TrackId) FROM Playlist, PlaylistTrack; --277202106 ?
 
+SELECT category,
+SUM(downloads) FROM
+fake_apps
+GROUP BY category;
+
 /* SELECT  P.Name, COUNT(PlaylistTrack.TrackId)
 FROM Playlist P, PlaylistTrack
 GROUP BY TrackId */
