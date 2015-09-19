48553a79c67010dcdc657f2230ff5acb193ab452
Chris Jarvis <clj@ollure.com>
2015-09-19 00:19:47 -0500

    reread 23 changed to Total Sales using SUM

--- 23.sql
+++ 23.sql
@@ -1,4 +1,4 @@
-SELECT I.BillingCountry, COUNT(I.BillingCountry)
+SELECT I.BillingCountry, SUM(I.Total) AS TotalSales
 FROM Invoice I
 GROUP BY I.BillingCountry
-ORDER BY COUNT(I.BillingCountry)
+ORDER BY SUM(I.Total)
