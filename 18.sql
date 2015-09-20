742a8f3210b97b469f2d51f8af5deff6c1b02639
Chris Jarvis <clj@ollure.com>
2015-09-19 22:04:46 -0500

    reworking 18

--- 18.sql
+++ 18.sql
@@ -1,6 +1,8 @@
-SELECT I.invoiceId, I.CustomerId, C.CustomerId,  C.SupportRepId AS E.EmployeeId COUNT(I.InvoiceLineId)
-FROM Customer C
-JOIN Customer
-ON  I.CustomerId == C.CustomerId
-GROUP BY I.invoiceId
+SELECT I.InvoiceId, I.CustomerId, C.CustomerId,  C.SupportRepId AS E.EmployeeId, E.FirstName, E.LastName, COUNT(I.InvoiceLineId)
+FROM Invoice I
+JOIN Customer C
+ON  C.CustomerId == I.CustomerId
+JOIN Employee E
+ON C.SupportRepId == E.EmployeeId
+GROUP BY EmployeeId
 
