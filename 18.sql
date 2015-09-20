d71dca77c1dc32cf9e6d0f089ec3ec7995b6fc93
Chris Jarvis <clj@ollure.com>
2015-09-19 22:27:07 -0500

    trying to use Round

--- 18.sql
+++ 18.sql
@@ -1,4 +1,4 @@
-SELECT I.InvoiceId, I.CustomerId, C.CustomerId,  C.SupportRepId AS E.EmployeeId, E.FirstName, E.LastName, COUNT(I.InvoiceLineId)
+SELECT  E.FirstName, E.LastName, SUM(ROUND(I.Total,0)) AS TotalSales --why wont ROUND(column_name, 2) work but 0 does?
 FROM Invoice I
 JOIN Customer C
 ON  C.CustomerId == I.CustomerId
