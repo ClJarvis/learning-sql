c5a5228b38f898581476aa1a6d6561f09ce1984a
Chris Jarvis <clj@ollure.com>
2015-09-20 23:21:58 -0500

    reworking 18

--- 18.sql
+++ 18.sql
@@ -1,3 +1,12 @@
+SELECT  E.FirstName, E.LastName, COUNT(I.InvoiceId) AS TotalSales
+FROM Invoice I
+JOIN Customer C
+ON  C.CustomerId == I.CustomerId
+JOIN Employee E
+ON C.SupportRepId == E.EmployeeId
+GROUP BY EmployeeId
+
+
 SELECT  E.FirstName, E.LastName, SUM(I.InvoiceLineId) AS TotalSales --why wont ROUND(column_name, 2) work but 0 does?
 FROM Invoice I
 JOIN Customer C
