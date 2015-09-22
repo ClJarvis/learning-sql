87c6d9e4896a04b4105690ed99b6429898522491
Chris Jarvis <clj@ollure.com>
2015-09-21 23:57:21 -0500

    refactoring 9

--- 9.sql
+++ 9.sql
@@ -1,4 +1,8 @@
-SELECT COUNT(I.InvoiceDate)
-FROM Invoice I;
+SELECT COUNT(I.InvoiceId)
+FROM Invoice I
+JOIN InvoiceLIne
+ON InvoiceLine.invoiceId == I.InvoiceId
+WHERE I.InvoiceDate LIKE '%2009%'
+GROUP BY I.Total
 
 NOT FINISHED NEED SALES TOTALS FOR EACH YEAR
