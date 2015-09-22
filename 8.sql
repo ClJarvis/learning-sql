72724d4368c8b5d0cb04bd3f345213fe5e8d2efe
Chris Jarvis <clj@ollure.com>
2015-09-21 23:12:47 -0500

    refactored 8 to add customerName

--- 8.sql
+++ 8.sql
@@ -1,6 +1,7 @@
-SELECT I.Total, I.BillingCountry, E.FirstName, E.LastName
+SELECT I.Total, C.FirstName || ' ' || C.LastName AS CustomerName, I.BillingCountry, E.FirstName || ' ' || E.LastName AS Sales_Agent
 FROM Invoice I
 JOIN Customer C
 ON C.CustomerId  == I.CustomerId
 JOIN Employee E ON
 E.EmployeeId == C.SupportRepId
+
